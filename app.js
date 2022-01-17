69d55f15bdff440af17415b1c74ba854d68344a8
JACustodioD <jacustodiod@hotmail.com>
2022-01-17 16:10:51 -0600

    Reponse locals

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.post("/users", function(req, res){
 app.post('/sessions', function(req, res){
   User.findOne({email:req.body.email, password:req.body.password}, function(err,user){
     req.session.user_id = user._id;
-    res.send("Aqui va ir la respuesta");
+    res.redirect("/app");
   });
 
 
